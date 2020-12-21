66457837c4a5834ba5847a25e8f1b83c40ebb5c1
Xceldeveloper <56355878+Xceldeveloper@users.noreply.github.com>
2020-12-21 10:59:34 +0100

    cool

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,27 @@
 <template>
-  <div>
-    
+  <div id="wrapper">
+    <component :is="selectedComponent"/>
   </div>
 </template>
 
 <script>
+import Home from '~/components/home.vue'
 export default {
-
+   components:{
+     'home': Home
+   },
+   data(){
+     return{
+       selectedComponent:'home'
+     }
+   }
 }
 </script>
+<style
+Home lang="scss" scoped>
+#wrapper{
+  width: 100vw;
+  height: 100vh;
+  overflow: auto;
+}
+</style>
